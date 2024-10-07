c50547dfa350910068f1770dec9fbf6eaae4611b
NTHuy652 <100992663+TanHuy2k2@users.noreply.github.com>
2024-10-07 16:53:53 +0700

    new

--- fontend/js/pose_estimation.js
+++ fontend/js/pose_estimation.js
@@ -22,6 +22,18 @@ function getCookies() {
     input_height.value = cookieObj['height'];
     input_weight.value = cookieObj['weight'];
 }
+
+function calculate_angle(a, b, c){
+
+    const radians = Math.atan2(c[1] - b[1], c[0] - b[0]) - Math.atan2(a[1] - b[1], a[0] - b[0]);
+    let angle = Math.abs(radians * 180.0 / Math.PI);
+
+    if (angle > 180.0) {
+        angle = 360 - angle;
+    }
+
+    return angle;
+}
     
 function onResults(results) {
 
@@ -36,9 +48,48 @@ function onResults(results) {
     
     // Only overwrite existing pixels.
     drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                     {color: '#00FF00', lineWidth: 1});
+                     {color: '#00FF00', lineWidth: 1, radius: 2});
     drawLandmarks(canvasCtx, results.poseLandmarks,
-                    {color: '#FF0000', lineWidth: 0.2});
+                    {color: '#FF0000', lineWidth: 0.2, radius: 2});
+
+    const landmarks = results.poseLandmarks;
+
+    if (landmarks) {
+
+        const right_shoulder = [landmarks[12].x, landmarks[12].y];
+        const left_shoulder = [landmarks[11].x, landmarks[11].y];
+
+        const right_elbow = [landmarks[14].x, landmarks[14].y];
+        const left_elbow = [landmarks[13].x, landmarks[13].y];
+
+        const right_wrist = [landmarks[16].x, landmarks[16].y];
+        const left_wrist = [landmarks[15].x, landmarks[15].y];
+
+        const right_hip = [landmarks[24].x, landmarks[24].y];
+        const left_hip = [landmarks[23].x, landmarks[23].y];
+
+        const leftShoulderDistance = Math.abs(left_shoulder[0] - left_hip[0]);
+        const rightShoulderDistance = Math.abs(right_shoulder[0] - right_hip[0]);
+                        
+        if (leftShoulderDistance < rightShoulderDistance){
+            shoulder = left_shoulder;
+            elbow = left_elbow;
+            wrist = left_wrist;
+        }else{
+            shoulder = right_shoulder;
+            elbow = right_elbow;
+            wrist = right_wrist;
+        }        
+
+        // Calculate angle
+        const angle = calculate_angle(shoulder, elbow, wrist);
+        console.log(angle);
+
+        canvasCtx.font = '18px Arial';
+        canvasCtx.fillStyle = '#00FF00';
+        canvasCtx.fillText(String(Math.round(angle)), Math.round(elbow[0] * canvasElement.width), Math.round(elbow[1] * canvasElement.height));    
+        canvasCtx.fillText("counter", 10, 20);    
+    }
 
     canvasCtx.restore();
 }
@@ -47,11 +98,11 @@ const pose = new Pose({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
 }});
 pose.setOptions({
-  modelComplexity: 0,
+  modelComplexity: 1,
   static_image_mode: false, 
   smoothLandmarks: true,
   minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5,
+  minTrackingConfidence: 0.7,
   selfieMode: false,
 });
 
